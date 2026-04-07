8567188254200dfeabef54e4b0ba2275393429be
selestinjohn <166312025+selestinjohn@users.noreply.github.com>
2026-04-07 18:30:56 +0300

    modified urls.py

--- voice_pay_backend/urls.py
+++ voice_pay_backend/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import path, include
-
+from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Voice Pay Backend is running successfully")
 
-
 urlpatterns = [
-    path("", home),
     path("admin/", admin.site.urls),
     path("api/", include("transactions.urls")),
+    path("", home),
 ]
